7c7efd140c75691d02660af559960bc7a2546c5d
Arthur Rezende <arthur-er@protonmail.com>
2024-02-20 14:07:09 -0300

    feat: basic node ts setup

--- adonis-typings/container.ts
+++
@@ -1,5 +0,0 @@
-declare module '@ioc:Adonis/Core/Application' {
-  export interface ContainerBindings {
-    // declare your container bindings here
-  }
-}
--- commands/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Export your commands using the full path to the build code
- * ex: '@org/package/build/commands/Command'
- */
-
-export default []
